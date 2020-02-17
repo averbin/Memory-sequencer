4c5584237797ca0cd5726f2e03f495a2cd222c52
Alexander <sunchos89@mail.ru>
2020-02-17 21:08:14 +0200

    Created toolbutton work in progress...

---
+++ toolButton.lua
@@ -0,0 +1,63 @@
+-----------------------------------------------------------------------------------------
+--
+-- toolButton.lua
+--
+-----------------------------------------------------------------------------------------
+
+local widget = require( "widget" )
+
+local TooButton = {}
+
+local options = {
+  group = rectGroup,
+  x = 0,
+  y = 0,
+  width = width,
+  height = height,
+  cornerRadius = cornerRadius,
+  fillColor = {1, 1, 1, 1},
+  strokeColor = {0.8, 0.8, 1, 1}
+  }
+
+function CreateButton(name, x, y, width, height, shape, cornerRadius)
+  local newButton = widget.newButton( 
+    {
+      id = name,
+      shape = shape,
+      cornerRadius = cornerRadius,
+      width = width,
+      height = height,
+      fillColor = { default={0, 0, 0, 1}, over={1, 1, 1, 1} },
+      strokeColor = { default={1, 1, 1, 1}, over={0.8, 0.8, 1, 1} },
+      onEvent = handleButtonEvent,
+      strokeWidth = 2
+    }
+  )
+  newButton.x = x
+  newButton.y = y
+  return newButton 
+end
+
+function CreateRect( options )
+  local newRect = display.newRoundedRect(
+    options.group,
+    options.x, options.y,
+    options.width, options.height, options.cornerRadius )
+  newRect.strokeWidth = 2
+  newRect:setFillColor( 1 , 1, 1, 1)
+  newRect:setStrokeColor( 0.8, 0.8, 1, 1 )
+  newRect.isVisible = false
+  return newRect
+end
+
+function ToolButton.new(group, id, 
+    x, y,
+    width, height,
+    shape, cornerRadius,
+    fillColor, strokeColor)
+  set = {}
+  setmetatable(set, {element, mark})
+  return set
+end
+
+return TooButton
