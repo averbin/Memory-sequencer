9f34dbd76ecb1c99f1bd0217df3dee5b45ff2694
Alexander <sunchos89@mail.ru>
2020-06-25 21:29:48 +0300

    Updated toolButton, now each button is pressed correctly.

--- toolButton.lua
+++ toolButton.lua
@@ -9,41 +9,56 @@ local widget = require( "widget" )
 tooButton = {}
 
 function tooButton.new( options )
-  options = options or {}
-  group = options.group
-  id = options.name or ""
-  x = options.x or 0
-  y = options.y or 0
-  width = options.width or 10
-  height = options.height or 10
-  cornerRadius = options.cornerRadius or 0
-  image = options.image or ""
+  options        = options                   or {}
+  group          = options.group             or nil
+  id             = options.name              or ""
+  
+  x              = options.x                 or 0
+  y              = options.y                 or 0
+  width          = options.width             or 10
+  height         = options.height            or 10
+  
+  cornerRadius   = options.cornerRadius      or 0
+  
+  fillColor      = options.fillColor         or {}
+  strokeColor    = options.strokeColor       or {}
+  
+  image          = options.image or ""
   isImageVisible = options.isImageVisible or false
   
   if not group then
     group = display.newGroup()
   end
   
-  function group:Blink()
-    transition.resume(id)
+  function group:Blink(obj)
+    print("Id: " .. obj.id)
+    transition.resume(obj.id)
   end
   
+  function touch( event )
+      group:Blink(event.target)
+      return true
+    end
+  
   local function onObjectTap( event )
-    group:Blink()
+    group:Blink(event.target)
     return true
   end
   
   local function CreateToolButton()
-    local button = display.newRoundedRect(group, x, y, width, height, cornerRadius)
-    button:setFillColor( 0.0, 0.0, 0.0, 1)
-    button:setStrokeColor(0.8, 0.8, 1, 1)
+    button = display.newRoundedRect(group, x, y, width, height, cornerRadius)
+    button.id = id
+    button:setFillColor(0.1, 0.1, 0.1, 1)
+    button:setStrokeColor(unpack(strokeColor))
     button.strokeWidth = 2
-    local insideRect = display.newRoundedRect(group, x, y, width, height, cornerRadius)
-    insideRect:setFillColor( 1, 1, 1, 1)
-    insideRect:setStrokeColor( 0.8, 0.8, 1, 1 )
+    button:addEventListener("tap", touch)
+    
+    insideRect = display.newRoundedRect(group, x, y, width, height, cornerRadius)
+    insideRect.id = id
+    insideRect:setFillColor(unpack(fillColor))
+    insideRect:setStrokeColor(unpack(strokeColor) )
     insideRect.strokeWidth = 2
-    transition.blink(insideRect, { tag = id, time = 1500, onRepeat = function ( event ) transition.pause() end })
-    button:addEventListener("tap", onObjectTap)
+    transition.blink(insideRect, { tag = id, time = 1500, onRepeat = function ( obj ) transition.pause(obj) end })
   end
   
   CreateToolButton()
