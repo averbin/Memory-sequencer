c8796d19ba16dfb0c8d07b8c28241c7448a784ab
Alexander <sunchos89@mail.ru>
2020-08-21 00:32:00 +0300

    added space

--- main.lua
+++ main.lua
@@ -7,6 +7,7 @@
 composer = require("composer")
 debugger = require("mobdebug.mobdebug")
 debugger.start()
+
 if( onIOS or onAndroid ) then
 	display.setStatusBar(display.HiddenStatusBar)  -- Hide that pesky bar	
 end
