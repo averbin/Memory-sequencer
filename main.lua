b6e3cb586d1d017d07501ed558b75458d54bafa8
averbin <alexander.verbin@swissmeda.com>
2022-07-03 19:16:03 +0300

    androidSystemUiVisibility set to immersiveSticky for ui

--- main.lua
+++ main.lua
@@ -6,7 +6,10 @@
 
 composer = require("composer")
 debugger = require("mobdebug.mobdebug")
+native = require("native")
 debugger.start()
+
+native.setProperty("androidSystemUiVisibility", "immersiveSticky")
 
 if( onIOS or onAndroid ) then
 	display.setStatusBar(display.HiddenStatusBar)  -- Hide that pesky bar	
