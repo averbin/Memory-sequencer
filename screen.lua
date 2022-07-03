5b742cc5f4a685a8d5373d5749642710c18e4c25
averbin <alexander.verbin@swissmeda.com>
2022-07-03 18:57:26 +0300

    changed screen table, added findSide devide heightByPieces and convertPersentToPixels

--- screen.lua
+++ screen.lua
@@ -10,32 +10,26 @@ screen.width = display.actualContentWidth
 screen.height = display.actualContentHeight
 screen.centerX = display.contentCenterX
 screen.centerY = display.contentCenterY
+screen.topSafetyArea = display.topStatusBarContentHeight
+screen.bottomSafetyArea = screen.height - screen.topSafetyArea
 
-function screen.new()
-  local set = {}
-  set.width = display.actualContentWidth
-  set.height = display.actualContentHeight
-  set.centerX = screen.centerX
-  set.centerY = screen.centerY
-
-  function set:findSide()
-    local side = 0
-    if screen.height > screen.width then
-      side = screen.width
-    else
-      side = screen.height
-    end
-
-    return side
+function screen:findSide()
+  local side = 0
+  if screen.height > screen.width then
+    side = screen.width
+  else
+    side = screen.height
   end
 
-  function set:convertPersentToPixels( persent )
-    return self.sideSize * ( persent / 100 )
-  end
+  return side
+end
 
-  set.sideSize = set:findSide()
+function screen:devideHeightByPieces( pieces )
+  return screen.height / pieces
+end
 
-  return set
+function screen:convertPersentToPixels( persent )
+  return self:findSide() * ( persent / 100 )
 end
 
 return screen
