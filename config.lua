c5b8146f85cd740bc29723088e76fe54c7fe82b5
averbin <alexander.verbin@swissmeda.com>
2022-07-27 22:25:06 +0300

    fixed configuration

--- config.lua
+++ config.lua
@@ -3,22 +3,10 @@
 -- https://docs.coronalabs.com/guide/basics/configSettings
 --
 
--- if aspect ratio is less than 1.5 then this resolution for a tablet.
-local aspectRatio = display.pixelHeight / display.pixelWidth
-if  aspectRatio < 1.5 then
-  --width for a tablet
-  width = 640
-else
-  --width for a phone
-  width = display.pixelWidth
-end
-
 application =
 {
   content =
   {
-    width = width,
-    height = width * ratio,
     scale = "letterbox",
     fps = 60,
   },
